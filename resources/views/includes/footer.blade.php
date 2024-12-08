398c70b4f2665007939509927816947c2a08847e
bryanozora19 <153263763+bryanozora19@users.noreply.github.com>
2024-12-08 22:14:10 +0700

    Delete resources directory

--- resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-gray-700 mt-20">
-    <div class="px-4 py-6 md:flex md:items-center md:justify-center" style="width: 100vw;">
-        <p style="color: white">©️ 2024 Music Store.</p>
-    </div>
-</footer>
